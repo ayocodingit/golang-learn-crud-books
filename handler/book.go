1606842cecaf6f4d4628dacdef70a01702ff16c0
ayocodingit <ayocodingit@gmail.com>
2021-09-21 09:12:31 +0700

    add response entity

--- handler/book.go
+++ handler/book.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"pustaka-api/book"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
@@ -26,8 +27,14 @@ func (h *bookHandler) Index(c *gin.Context) {
 		return
 	}
 
+	var booksResponse []book.BookResponse
+
+	for _, item := range books {
+		booksResponse = append(booksResponse, bookResponse(item))
+	}
+
 	c.JSON(http.StatusOK, gin.H{
-		"data": books,
+		"data": booksResponse,
 	})
 }
 
@@ -59,4 +66,30 @@ func (h *bookHandler) Store(c *gin.Context) {
 	c.JSON(http.StatusCreated, gin.H{
 		"data": book,
 	})
+}
+
+func (h *bookHandler) Show(c *gin.Context) {
+	idString := c.Param("id")
+	id, err := strconv.Atoi(idString)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"errors": err,
+		})
+	}
+	book, err := h.bookService.Show(id)
+
+	c.JSON(http.StatusOK, gin.H{
+		"data": bookResponse(book),
+	})
+}
+
+func bookResponse(b book.Book) book.BookResponse {
+	return book.BookResponse{
+		ID:          b.ID,
+		Title:       b.Title,
+		Price:       b.Price,
+		Description: b.Description,
+		Rating:      b.Rating,
+		Discount:    b.Discount,
+	}
 }
--- main.go
+++ main.go
@@ -30,6 +30,7 @@ func main() {
 
 	v1.GET("/", handler.HomeHandler)
 	v1.GET("/books", bookHandler.Index)
+	v1.GET("/books/:id", bookHandler.Show)
 	v1.POST("/books", bookHandler.Store)
 	router.Run()
 }
